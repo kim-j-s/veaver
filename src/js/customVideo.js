831eeeb6500608f1ec2a2a0ba95fac35a2fbcf9a
Namuk Kang <namuk@Namukui-MacBook-Pro.local>
2021-03-09 15:22:49 +0900

    knu

--- src/js/customVideo.js
+++ src/js/customVideo.js
@@ -114,6 +114,7 @@
             if (fe == 'm3u8') {
                 if (video.canPlayType('application/vnd.apple.mpegurl')) {
                     video.setAttribute('playsinline', true);
+                    video.setAttribute('webkit-playsinline', true);
                     video.src = videoSrc;
                 } else if (Hls.isSupported()) {
                     const hls = new Hls();
