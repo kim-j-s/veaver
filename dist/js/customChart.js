f358d8b8123349025c3e89d9c4769b34cf9bbcfc
LAPTOP-JAJQD1C5\jihyu <storrymn@gmail.com>
2021-05-06 12:16:58 +0900

    차트 내 bg 투명도 수정적용

--- dist/js/customChart.js
+++ dist/js/customChart.js
@@ -71,7 +71,7 @@
                                  yMax: obj.cfValue[3],
                                  //초과 영역 - 최소값
                                  yMin: obj.cfValue[2],
-                                 backgroundColor: "rgba(255, 115, 0, 0.3)"
+                                 backgroundColor: "rgba(255, 115, 0, 0.08)"
                              },
                              // 안정 영역
                              {
@@ -84,7 +84,7 @@
                                  yMax: obj.cfValue[2],
                                  //안정 영역 - 최소값
                                  yMin: obj.cfValue[1],
-                                 backgroundColor: "rgba(255, 191, 0, 0.3)"
+                                 backgroundColor: "rgba(255, 191, 0, 0.08)"
                              },
                              // 미달 영역
                              {
@@ -97,7 +97,7 @@
                                  yMax: obj.cfValue[1],
                                  //미달 영역 - 최소값
                                  yMin: obj.cfValue[0],
-                                 backgroundColor: "rgba(12, 166, 234, 0.3)"
+                                 backgroundColor: "rgba(12, 166, 234, 0.08)"
                              },
                          ]
                      }
